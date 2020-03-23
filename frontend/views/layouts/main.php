9fefa7a8841618d303bf0c8b1972cc8f262c5bd4
gghtrt520 <zhanglei520sl@126.com>
2020-03-24 07:12:28 +0800

    add

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -36,13 +36,13 @@ AppAsset::register($this);
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['site/index']],
-        ['label' => 'About', 'url' => ['site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => '首页', 'url' => ['site/index']],
+        ['label' => '网上纪念馆', 'url' => ['site/about']],
+        ['label' => '快速建馆', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        $menuItems[] = ['label' => '注册', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => '登录', 'url' => ['/site/login']];
     } else {
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
