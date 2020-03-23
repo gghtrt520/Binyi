0d88c2f722d67a50626aafd97652faf0ad610ede
gghtrt520 <zhanglei520sl@126.com>
2020-03-24 07:08:07 +0800

    add

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -10,32 +10,24 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<script src="http://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js"></script>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
 
     <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                    <br>
-                    Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
-                </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+        <div id="login_container">
         </div>
     </div>
 </div>
+<script>
+    var obj = new WxLogin({
+        self_redirect:false,
+        id:"login_container", 
+        appid: '<?= Html::encode(Yii::$app->params['AppID'])?>', 
+        scope: "snsapi_login", 
+        redirect_uri: "<?= Html::encode(urlencode('http://192.168.50.125/site/index'))?>",
+        state: "",
+        style: "",
+        href: ""
+    });
+</script>
