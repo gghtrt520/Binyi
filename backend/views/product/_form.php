0e9b87e85e5b23fd826184d96fe8228cb43dad81
root <root@localhost.localdomain>
2020-03-23 14:54:06 +0800

    delete time input

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -22,10 +22,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'norms')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
