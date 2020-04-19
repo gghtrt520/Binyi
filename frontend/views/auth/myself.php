aa191dd3adc2a50e9bd2d880933c775c4f493365
wish <weishiyouxiang@126.com>
2020-04-19 10:54:50 +0800

    my

--- frontend/views/auth/myself.php
+++ frontend/views/auth/myself.php
@@ -8,7 +8,7 @@ AppAsset::register($this);
 AppAsset::addCss($this, "css/my.css");
 AppAsset::addScript($this, "js/my.js");
 ?>
-<div class="main container">
+<div class="main mycontainer">
     <div class="page-title cf">
         <h2 class="f_l"><i class="icon-back-img2 icon-size-40-50 icon-school-page"></i>我的纪念馆</h2>
         <button class="creatBtn">创建纪念馆</button>
@@ -17,34 +17,34 @@ AppAsset::addScript($this, "js/my.js");
         <ul class="teacher-ul cf">
             <li class="f_l">
                 <div class="div-teacher-info">
-                    <div class="img-div-teacher"><a href=""><img src="img/04.png" alt=""></a></div>
+                    <div class="img-div-teacher"><a href=""><img src="/img/04.png" alt=""></a></div>
                     <h6>杨袖珠</h6>
                 </div>
             </li><li class="f_l">
                 <div class="div-teacher-info">
-                    <div class="img-div-teacher"><a href=""><img src="img/04.png" alt=""></a></div>
+                    <div class="img-div-teacher"><a href=""><img src="/img/04.png" alt=""></a></div>
                     <h6>杨袖珠</h6>
                 </div>
             </li><li class="f_l">
                 <div class="div-teacher-info">
-                    <div class="img-div-teacher"><a href=""><img src="img/04.png" alt=""></a></div>
+                    <div class="img-div-teacher"><a href=""><img src="/img/04.png" alt=""></a></div>
                     <h6>杨袖珠</h6>
                 </div>
             </li><li class="f_l">
                 <div class="div-teacher-info">
-                    <div class="img-div-teacher"><a href=""><img src="img/04.png" alt=""></a></div>
+                    <div class="img-div-teacher"><a href=""><img src="/img/04.png" alt=""></a></div>
                     <h6>杨袖珠</h6>
                 </div>
             </li>
             <li class="f_l">
                 <div class="div-teacher-info">
-                    <div class="img-div-teacher"><img src="img/zyxt7.png" alt=""></div>
+                    <div class="img-div-teacher"><img src="/img/zyxt7.png" alt=""></div>
                     <h6>杨袖珠</h6>
                 </div>
             </li>
             <li class="f_l">
                 <div class="div-teacher-info">
-                    <div class="img-div-teacher"><img src="img/zyxt7.png" alt=""></div>
+                    <div class="img-div-teacher"><img src="/img/zyxt7.png" alt=""></div>
                     <h6>杨袖珠</h6>
                 </div>
             </li>
