6ad84991235a9fca254173029b08b7469d652788
gghtrt520 <zhanglei520sl@126.com>
2020-04-19 18:38:40 +0800

    add

---
+++ frontend/controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+
+class AuthController extends Controller
+{
+
+
+
+    public function actionMyself()
+    {
+        return $this->render('myself');
+    }
+
+    
+}
