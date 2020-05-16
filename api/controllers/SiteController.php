d706125f18a1ee2cf52fcaea4d552e082ae2a305
gghtrt520 <zhanglei520sl@126.com>
2020-05-16 23:51:57 +0800

    xiugai

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -95,10 +95,9 @@ class SiteController extends \yii\rest\Controller
 
     public function decryptData($session_key,$data,$iv)
     {
-        $out  = [];
         $pc   = new \common\models\WXBizDataCrypt($this->AppID, $session_key);
-        $code = $pc->decryptData($data, $iv, $out );
-        return $out;
+        $code = $pc->decryptData($data, $iv, $out);
+        return json_decode($out,true);
     }
 
 
