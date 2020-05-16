3869416019bc56fe7f2688d66718d452eb06e652
gghtrt520 <zhanglei520sl@126.com>
2020-05-16 21:00:18 +0800

    添加授权

--- frontend/controllers/AuthController.php
+++ frontend/controllers/AuthController.php
@@ -7,7 +7,16 @@ use yii\web\Controller;
 
 class AuthController extends Controller
 {
+    $login_url = 'https://open.weixin.qq.com/connect/qrconnect?appid=wx07a5baa6f39cd947&redirect_uri=https://xcx.xhbinyi.com/site/wxcallback&response_type=code&scope=snsapi_login&state=STATE';
 
+    public function  init()
+    {
+        if(Yii::$app->user->isGest){
+            $this->redirect($login_url);
+        }else{
+            parent::init();
+        }
+    }
 
 
     public function actionMyself()
