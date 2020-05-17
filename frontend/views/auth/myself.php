2f93c0301fa575ac0907e17e7cfcbad45c8593bb
gghtrt520 <zhanglei520sl@126.com>
2020-05-17 23:18:37 +0800

    修改

--- frontend/views/auth/myself.php
+++ frontend/views/auth/myself.php
@@ -2,6 +2,7 @@
 
 use frontend\assets\AppAsset;
 use yii\helpers\Html;
+use  yii\helpers\Url;
 
 $this->title = '天堂纪念馆';
 AppAsset::register($this);
@@ -19,7 +20,7 @@ AppAsset::addScript($this, "js/my.js");
             <?php foreach ($room as $value) : ?>
                 <li class="f_l">
                     <div class="div-teacher-info">
-                        <div class="img-div-teacher"><a href=""><img src="<?= Html::encode($value->avatar_url) ?>" alt=""></a></div>
+                        <div class="img-div-teacher"><a href="<?=Url::toRoute(['site/about','id' =>$value->id]);?>"><img src="<?= Html::encode($value->avatar_url) ?>" alt=""></a></div>
                         <h6><?= Html::encode($value->name) ?></h6>
                     </div>
                 </li>
