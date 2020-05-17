9fdbef261257c53cfc0823048fe410b678477e1c
gghtrt520 <zhanglei520sl@126.com>
2020-05-17 21:44:08 +0800

    add

--- frontend/controllers/AuthController.php
+++ frontend/controllers/AuthController.php
@@ -52,5 +52,37 @@ class AuthController extends Controller
         ]);
     }
 
+    public function actionAdd()
+    {
+        $model  = new \common\models\Room();
+        $model->user_id = Yii::$app->user->identity->id;
+        $model->avatar_url = Yii::$app->request->post('avatar_url');
+        $model->name = Yii::$app->request->post('name');
+        $model->gender = Yii::$app->request->post('gender');
+        $model->birthdate = Yii::$app->request->post('birthdate');
+        $model->death = Yii::$app->request->post('death');
+        $model->description = Yii::$app->request->post('description');
+        $model->age = Yii::$app->request->post('age');
+        $model->province = Yii::$app->request->post('province');
+        $model->city = Yii::$app->request->post('city');
+        $model->area = Yii::$app->request->post('area');
+        $model->religion = Yii::$app->request->post('religion');
+        $model->category = Yii::$app->request->post('category');
+        $model->is_pay     = Yii::$app->request->post('is_pay');
+        $model->rule = Yii::$app->request->post('rule');
+        if ($model->save()) {
+            return  $this->asJson([
+                'code'    => 1,
+                'message' => '操作成功',
+                'data'    => $model
+            ]);
+        } else {
+            return  $this->asJson([
+                'code'    => 0,
+                'message' =>$this->getErrorMessage($model),
+            ]);
+        }
+    }
+
     
 }
