0cc2bf52a9a1fe8b1bed4dd50068509f3c8998a0
gghtrt520 <zhanglei520sl@126.com>
2020-05-17 20:01:08 +0800

    修改

--- frontend/controllers/AuthController.php
+++ frontend/controllers/AuthController.php
@@ -9,17 +9,19 @@ class AuthController extends Controller
 {
     private $login_url = 'https://open.weixin.qq.com/connect/qrconnect?appid=wx07a5baa6f39cd947&redirect_uri=https://xcx.xhbinyi.com/site/wxcallback&response_type=code&scope=snsapi_login&state=STATE';
 
-    public function  init()
+    
+    public function beforeAction($action)
     {
         if(Yii::$app->user->isGuest){
             return $this->redirect($this->login_url);
         }
-        parent::init();
     }
-
 
     public function actionMyself()
     {
+        if(Yii::$app->user->isGuest){
+            return $this->redirect($this->login_url);
+        }
         $user_id = Yii::$app->user->identity->id;
         $room    = \common\models\Room::find()->where(['user_id'=>$user_id])->all();
         return $this->render('myself',['room'=>$room]);
@@ -27,6 +29,9 @@ class AuthController extends Controller
 
     public function actionCreate()
     {
+        if(Yii::$app->user->isGuest){
+            return $this->redirect($this->login_url);
+        }
         return $this->render('create');
     }
 
